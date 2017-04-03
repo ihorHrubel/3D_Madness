c15a6e3c2e46edbbcedf2fb84cbd959cc57ee475
Ihor <ihora030996@gmail.com>
2017-04-03 15:49:42 +0300

    Added login and registration

---
+++ 3D_Madness_site/WebApplication2/Models/IdentityModels.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace WebApplication2.Models
+{
+    // Чтобы добавить данные профиля для пользователя, можно добавить дополнительные свойства в класс ApplicationUser. Дополнительные сведения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=317594.
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
+        }
+
+    }
+
+    public class DModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Weight { get; set; }
+        public string Version { get; set; }
+        public string Color { get; set; }
+        public string Style { get; set; }
+
+    }
+
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int DModelId { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<DModel> DModels { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+        
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
